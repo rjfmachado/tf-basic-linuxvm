3593b5d9964f004a3564d4bcdf4b6a18aaf1fbb1
rjfmachado <rjfmachado@users.noreply.github.com>
2020-06-27 11:09:44 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# tf-basic-linuxvm
+# Basic Linux VM
+
+Linux VM with:
+- Ubuntu 18.04
+- SSH Authentication only
+- No public ip
+- Standard B1s SKU
---
+++ module.tf
@@ -0,0 +1,41 @@
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-${var.name}"
+  location            = var.location
+  resource_group_name = var.rg
+
+  ip_configuration {
+    name                          = "ipconfig-${var.name}"
+    subnet_id                     = var.subnetid
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.name
+  resource_group_name = var.rg
+  location            = var.location
+  size                = var.size
+  admin_username      = var.adminuser
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.adminuser
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  disable_password_authentication = true
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
