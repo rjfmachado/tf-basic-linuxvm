33ffeda65a60f1c7cf74847404868466353b6b5b
rjfmachado <rjfmachado@users.noreply.github.com>
2020-11-09 15:49:10 +0000

    removing provider constraints

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    azurerm = "~> 2.0"
-  }
-}
+# terraform {
+#   required_version = ">= 0.13"
+#   required_providers {
+#     azurerm = "~> 2.0"
+#   }
+# }
